ff1209b50d0b239f7cb9920801fb23590f77cd1d
Viperexz <osferhoc54@gmail.com>
2023-09-24 16:12:01 -0500

    Nuevo

--- index.php
+++ index.php
@@ -1,24 +1,52 @@
 <?php
-require_once './Controlador/clsRegistrarParticipante.php';
+require_once 'Controlador/clsManejoDatos.php'; // Asegúrate de incluir la clase clsManejoDatos
+
+class clsRegistrarParticipante {
+private $conexion;
+private $manejoDatos;
+
+public function __construct() {
+$this->manejoDatos = new \Controlador\clsManejoDatos();
+$this->conexion = $this->manejoDatos->getConexion();
+}
+
+public function registrar($nombre, $apellidos, $cedula, $fechaNacimiento, $correo, $telefono) {
+$nombre = $this->conexion->real_escape_string($nombre);
+$apellidos = $this->conexion->real_escape_string($apellidos);
+$cedula = $this->conexion->real_escape_string($cedula);
+$fechaNacimiento = $this->conexion->real_escape_string($fechaNacimiento);
+$correo = $this->conexion->real_escape_string($correo);
+$telefono = $this->conexion->real_escape_string($telefono);
+
+$sql = "INSERT INTO participantes (parCedula, parNombre, parFechaNacimiento, parTelefono, parCorreo)
+VALUES ('$cedula','$nombre + $apellidos',  '$fechaNacimiento',  '$telefono','$correo')";
+
+if ($this->manejoDatos->ejecutar($sql)) {
+// Registro exitoso, redirige a la página anterior con un mensaje de éxito
+header("Location: index.php?mensaje=exito");
+exit;
+} else {
+// Error en el registro, redirige a la página anterior con un mensaje de error
+header("Location: index.php?mensaje=error");
+exit;
+}
+}
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $nombre = $_POST['Nombre'];
-    $apellidos = $_POST['Apellidos'];
-    $cedula = $_POST['Cedula'];
-    $fechaNacimiento = $_POST['FN'];
-    $correo = $_POST['Correo'];
-    $telefono = $_POST['Telefono'];
+$nombre = $_POST['Nombre'];
+$apellidos = $_POST['Apellidos'];
+$cedula = $_POST['Cedula'];
+$fechaNacimiento = $_POST['FN'];
+$correo = $_POST['Correo'];
+$telefono = $_POST['Telefono'];
 
-    // Validar que todos los campos estén llenos
-    if (empty($nombre) || empty($apellidos) || empty($cedula) || empty($fechaNacimiento) || empty($correo) || empty($telefono)) {
-        echo "Todos los campos son obligatorios. Por favor, complete todos los campos.";
-    } else {
-        // Crear una instancia de la clase clsRegistrarParticipante
-        $registrador = new clsRegistrarParticipante();
-
-        // Llamar al método registrar para insertar en la base de datos
-        $registrador->registrar($nombre, $apellidos, $cedula, $fechaNacimiento, $correo, $telefono);
-    }
+if (empty($nombre) || empty($apellidos) || empty($cedula) || empty($fechaNacimiento) || empty($correo) || empty($telefono)) {
+echo "Todos los campos son obligatorios. Por favor, complete todos los campos.";
+} else {
+$registrador = new clsRegistrarParticipante();
+$registrador->registrar($nombre, $apellidos, $cedula, $fechaNacimiento, $correo, $telefono);
+}
 }
 ?>
 
@@ -42,7 +70,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body>
 <div class="container" id="advanced-search-form">
     <h2>Registre sus datos</h2>
-    <form id="registro-form" method="POST" action="./Controlador/clsRegistrarParticipante.php">
+    <form id="registro-form" method="POST" action="">
         <div class="form-group">
             <label for="Nombre">Nombres</label>
             <input type="text" class="form-control" placeholder="Nombres" id="Nombre" name="Nombre" required>
@@ -102,3 +130,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </div>
 </body>
 </html>
+
+
+
+<
