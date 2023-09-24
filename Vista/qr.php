15b7bbce91639f709fba0afde44d019de0f7a7d7
Viperexz <osferhoc54@gmail.com>
2023-09-24 18:11:09 -0500

    Generar codigo QR

---
+++ Vista/qr.php
@@ -0,0 +1,23 @@
+<?php
+// Incluir la biblioteca PHP QR Code
+include('../phpqrcode/qrlib.php');
+
+// Definir los dos parámetros que deseas codificar en el QR
+$parametro1 = "Valor1";
+$parametro2 = "Valor2";
+
+// Combinar los dos parámetros en una cadena si es necesario
+$datos = $parametro1 . " | " . $parametro2;
+
+// Directorio donde se guardará el archivo QR
+$dir = 'qrcodes/';
+
+// Nombre del archivo QR
+$archivoQR = $dir . 'qr_parametros.png';
+
+// Crear el código QR
+QRcode::png($datos, $archivoQR);
+
+// Mostrar la imagen del código QR en la página
+echo '<img src="' . $archivoQR . '" alt="Código QR con parámetros">';
+?>
