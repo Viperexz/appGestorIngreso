b5fc82e70a3432e147605346f7742e3badf2924d
Viperexz <osferhoc54@gmail.com>
2023-09-25 12:13:20 -0500

    Serivico Mail

--- Vista/qr.php
+++ Vista/qr.php
@@ -21,10 +21,13 @@
     <div class="row">
         <div class="col-md-6">
             <?php
-            if (isset($_GET['Nombre']) && isset($_GET['Apellidos']) && isset($_GET['Cedula'])&& isset($_GET['Correo'])) {
+            require '../PHPMailer/src/PHPMailer.php';
+            require '../PHPMailer/src/SMTP.php';
+            if (isset($_GET['Nombre']) && isset($_GET['Apellidos']) && isset($_GET['Cedula']) && isset($_GET['Correo'])) {
                 $nombre = $_GET['Nombre'];
                 $apellidos = $_GET['Apellidos'];
                 $cedula = $_GET['Cedula'];
+                $correoDestino = $_GET['Correo'];
 
                 // Utiliza $cedula para generar el código QR
                 require_once '../phpqrcode/qrlib.php';
@@ -32,61 +35,47 @@
                 // Ruta donde deseas guardar el archivo QR
                 $archivoQR = "qrcodes/$cedula.png";
 
-                // Tamaño personalizado para el código QR (por ejemplo, 300x300 píxeles)
-                $tamaño = 300;
+                // Tamaño personalizado para el código QR (por ejemplo, 150x150 píxeles)
+                $tamaño = 150;
 
                 // Genera el código QR con el tamaño personalizado
                 QRcode::png($cedula, $archivoQR, QR_ECLEVEL_L, $tamaño);
 
-                // Muestra el QR en la página
-                echo '<img src="' . $archivoQR . '" alt="Código QR" width="' . $tamaño . '" height="' . $tamaño . '">';
-            } else {
-                echo "No se proporcionaron los datos necesarios para generar el código QR.";
+                // Incluye la biblioteca PHPMailer
+                require 'PHPMailer/PHPMailer.php';
+                require 'PHPMailer/SMTP.php';
+
+                // Crea una instancia de PHPMailer
+                $mail = new PHPMailer\PHPMailer\PHPMailer();
+
+                // Configura el servidor SMTP
+                $mail->isSMTP();
+                $mail->Host = 'mail.parchefest.co '; // Cambia esto al servidor SMTP correcto
+                $mail->SMTPAuth = true;
+                $mail->Username = '_mainaccount@parchefest.co'; // Cambia esto a tu correo
+                $mail->Password = '534R541%l'; // Cambia esto a tu contraseña
+                $mail->SMTPSecure = 'tls';
+                $mail->Port = 465;
+
+                // Configura el correo electrónico
+                $mail->setFrom('wwparc@parchefest.co', 'Parchefest'); // Cambia esto a tu nombre y correo
+                $mail->addAddress($correoDestino, $nombre); // Agrega el destinatario y su nombre
+                $mail->Subject = 'Código QR';
+                $mail->Body = "Hola $nombre $apellidos,\n\nAdjunto encontrarás tu código QR.";
+
+                // Adjunta el código QR al correo
+                $mail->addAttachment($archivoQR, 'codigo_qr.png');
+
+                // Envía el correo electrónico
+                if ($mail->send()) {
+                    echo "<h3>Se registraron tus datos correctamente, $nombre $apellidos.</h3>";
+                    echo "<h4>El código QR se envió a tu correo electrónico.</h4>";
+                } else {
+                    echo "<h3>Error al enviar el correo electrónico.</h3>";
+                }
             }
-
-            $correoDestino = $_GET['Correo'];
-
-            // Asunto del correo electrónico
-            $asunto = "Código QR";
-
-            // Mensaje del correo electrónico
-            $mensaje = "Hola $nombre $apellidos,\n\nAdjunto encontrarás tu código QR.";
-
-            // Cabecera del correo electrónico
-            $cabecera = "From: wwparc@parchefest.co";
-            $cabecera .= "\r\nMIME-Version: 1.0";
-            $cabecera .= "\r\nContent-Type: multipart/mixed; boundary=\"PHP-mixed-" . md5(time()) . "\"";
-
-            // Contenido del correo electrónico (archivo adjunto)
-            $contenidoCorreo = "--PHP-mixed-" . md5(time()) . "\r\n";
-            $contenidoCorreo .= "Content-Type: multipart/alternative; boundary=\"PHP-alt-" . md5(time()) . "\"\r\n\r\n";
-            $contenidoCorreo .= "--PHP-alt-" . md5(time()) . "\r\n";
-            $contenidoCorreo .= "Content-Type: text/plain; charset=\"utf-8\"\r\n";
-            $contenidoCorreo .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
-            $contenidoCorreo .= $mensaje . "\r\n\r\n";
-            $contenidoCorreo .= "--PHP-alt-" . md5(time()) . "--\r\n\r\n";
-            $contenidoCorreo .= "--PHP-mixed-" . md5(time()) . "\r\n";
-            $contenidoCorreo .= "Content-Type: application/octet-stream; name=\"codigo_qr.png\"\r\n";
-            $contenidoCorreo .= "Content-Transfer-Encoding: base64\r\n";
-            $contenidoCorreo .= "Content-Disposition: attachment\r\n\r\n";
-            $contenidoCorreo .= chunk_split(base64_encode(file_get_contents($archivoQR))) . "\r\n";
-            $contenidoCorreo .= "--PHP-mixed-" . md5(time()) . "--";
-
-            // Envía el correo electrónico
-            if (mail($correoDestino, $asunto, $contenidoCorreo, $cabecera)) {
-                echo "<h3>Se registraron tus datos correctamente, $nombre $apellidos.</h3>";
-                echo "<h4>El código QR se envió a tu correo electrónico.</h4>";
-            } else {
-                echo "<h3>Error al enviar el correo electrónico.</h3>";
-            }
-
-
             ?>
-        </div>
-        <div class="col-md-4">
-            <h3> Se registraron tus datos correctamente, <?php echo $nombre . ' ' . $apellidos; ?></h3>
-            <h3>El QR se envió a tu correo</h3>
-            <h4> Recuerda presentarlo en el evento.</h4>
+
         </div>
     </div>
 </div>
