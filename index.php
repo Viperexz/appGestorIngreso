509f4679115840ca028da3366e6c3229ed34781c
Viperexz <osferhoc54@gmail.com>
2023-09-25 11:51:22 -0500

    Solucion

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $sql = "INSERT INTO participante VALUES ('$cedula', '$nombre $apellidos', '$fech
 
 if ($this->manejoDatos->ejecutar($sql)) {
 // Registro exitoso, redirige a la página anterior con un mensaje de éxito
-    header("Location: qr.php?Nombre=$nombre&Apellidos=$apellidos&Cedula=$cedula");
+    header("Location: Vista/qr.php?Nombre=$nombre&Apellidos=$apellidos&Cedula=$cedula");
 
     exit;
 } else {
