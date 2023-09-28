6843330a79860b4cdb7288ab74ead7d04525af77
Viperexz <osferhoc54@gmail.com>
2023-09-28 15:52:47 -0500

    camara 2 push

--- Vista/escaner.php
+++ Vista/escaner.php
@@ -193,17 +193,25 @@ class clsVerificarQr
     }
     }
 
-      function setResult(label, result) {
-          console.log(result.data);
-          if (scanningActive) {
-              // Realizar acciones adicionales solo si el escaneo está activo
-              sendData(result);
-              label.textContent = result.data;
-              // Detener el escaneo después de procesar el primer resultado
-              scanningActive = false;
-              scanner.stop();
-          }
-      }
+    function setResult(label, result) {
+        if (result && result.data) {
+            console.log(result.data);
+            label.textContent = result.data;
+
+            if (scanningActive) {
+                // Realizar acciones adicionales solo si el escaneo está activo
+                sendData(result);
+
+                // Detener el escaneo después de procesar el primer resultado
+                scanningActive = false;
+                scanner.stop();
+            }
+        } else {
+            console.log("No se detectó información válida en el resultado.");
+        }
+    }
+
+    // Resto de tu código (Web Cam Scanning) sigue igual...
 
     // ####### Web Cam Scanning #######
 
