39b095fd29bad08d04d4fd1d8dfc173fe3c20c80
Viperexz <osferhoc54@gmail.com>
2023-09-29 18:33:41 -0500

    Pagina consultas

--- Vista/escaner.php
+++ Vista/escaner.php
@@ -115,7 +115,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <a class="nav-link" href="escaner.php">Escaner</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="consultar.php">Ayuda</a>
+                    <a class="nav-link" href="consultar.php">Consultar</a>
                 </li>
             </ul>
         </div>
